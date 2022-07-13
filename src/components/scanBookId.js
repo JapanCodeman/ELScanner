2796206f31eea0e480dfb5bab704bd70db243785
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2022-07-13 13:14:02 +0900

    removed another qr-react-scanner module that didn't work; reverted

--- src/components/scanBookId.js
+++ src/components/scanBookId.js
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+import React, { Component } from 'react';
 
-const Test = (props) => {
-  const [data, setData] = useState('No result');
-
-  return (
-    <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      />
-      <p>{data}</p>
-    </>
-  );
-};
-
-export default Test;
+export default class ScanBookID extends Component {
+  render () {
+    return (
+      <div>
+        Scan Book ID component goes here...
+      </div>
+    );
+  }
+}
