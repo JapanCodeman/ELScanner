87f0b53fd739961c5eebd708c7fb3d19a17188d3
JapanCodeman <ryansurdick@MacBook-Pro.local>
2022-09-18 15:34:48 +0900

    setting up grid on scanBookId.js

--- src/components/scanBookId.js
+++ src/components/scanBookId.js
@@ -9,6 +9,7 @@ export default class scanBookId extends Component {
     this.state = {
     }
 
+    this.rescan = this.rescan.bind(this)
     this.updateBookId = this.updateBookId.bind(this)
   }
   
@@ -26,27 +27,34 @@ export default class scanBookId extends Component {
     })
   }
 
+  rescan() {
+    // eslint-disable-next-line no-unused-expressions
+    window.location.reload()
+  }
+
   render () {
     return (
       <div className='scan-book-wrapper'>
         <Scanner updateBookId = {(bookID) => this.updateBookId(bookID)}/>
         {this.state.book != null ? 
         <div className='scan-result-table'>
-          <div className='scan-result__upc-label'>UPC</div>
+          <button onClick={this.rescan} className='scan-result__restart'>Wrong title?</button>
+          <label className='scan-result__upc-label'>UPC</label>
           <div className='scan-result__upc'>{this.state.book.data.upc}</div>
-          <div className='scan-result__title-label'>Title</div>
-          <div className='scan-result__title'>{this.state.book.data.title} 
-          <div className='scan-result__publisher-label'>Publisher</div>
+          <label className='scan-result__title-label'>Title</label>
+          <div className='scan-result__title'>{this.state.book.data.title}</div>
+          <label className='scan-result__publisher-label'>Publisher</label>
           <div className='scan-result__publisher'>{this.state.book.data.publisher}</div>
-          <div className='scan-result__author-label'>Author</div>
+          <label className='scan-result__author-label'>Author</label>
           <div className='scan-result__author'>{this.state.book.data.author}</div>
-          <div className='scan-result__word-count-label'>Word Count</div>
+          <label className='scan-result__word-count-label'>Word Count</label>
           <div className='scan-result__word-count'>{this.state.book.data.wordCount}</div>
-          <div className='scan-result__status-label'>Status</div>
+          <label className='scan-result__status-label'>Status</label>
           <div className='scan-result__status'>{this.state.book.data.status}</div>
-          <div className='scan-result__current-holder-label'>{this.state.book.data.currentHolder}</div>
+          <label className='scan-result__current-holder-label'>Held by</label>
+          <div className='scan-result__current-holder'>{this.state.book.data.currentHolder}</div>
         </div>
-      </div>
+          
           : 
           
           null }
