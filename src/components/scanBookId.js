dc6283bcfa3625bc703f5c849c4587403296c86b
JapanCodeman <ryansurdick@MacBook-Pro.local>
2022-09-19 12:09:12 +0900

    Converted scanBookId.js from class to functional component. Added useNavigate function to SmallerGreenButtons' clickHandler prop

--- src/components/scanBookId.js
+++ src/components/scanBookId.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import PageTitler from './helpers/pageTitler';
 import SmallerGreenButton from './helpers/smallerGreenButton';
 import Scanner from './scanner';
 
-export default class scanBookId extends Component {
-  constructor(props) {
-    super(props) 
+function ScanBookId() {
 
-    this.state = {
+    const [book, updateBook] = useState({
       upc : "",
       title : "",
       publisher : "",
@@ -17,19 +16,15 @@ export default class scanBookId extends Component {
       wordCount : 0,
       status : "",
       currentHolder : ""
-    }
+    });
 
-    this.checkBookIn = this.checkBookIn.bind(this);
-    this.checkBookOut = this.checkBookOut.bind(this);
-    this.rescan = this.rescan.bind(this);
-    this.updateBookId = this.updateBookId.bind(this);
-  }
+  const navigate = useNavigate()
   
-  updateBookId(bookID) {
+  const updateBookId = (bookID) => {
     axios
     .get(`http://127.0.0.1:5000/retrieve-book-info/${bookID}`)
     .then(book => {
-      this.setState({
+      updateBook({
         ...book.data
       })
     })
@@ -38,51 +33,51 @@ export default class scanBookId extends Component {
     })
   }
 
-  checkBookIn() {
+  const checkBookIn = () => {
     axios
-    .patch(`http://127.0.0.1:5000/${this.state.upc}`)
+    .patch(`http://127.0.0.1:5000/${book.upc}`)
   }
 
-  checkBookOut() {
-    this.props.history.push('/scan-student-id');
+  const checkBookOut = () => {
+    console.log('checkBooktOut clicked');
+    navigate('/scan-student-id');
   }
 
-  rescan() {
+  const rescan = () => {
     // eslint-disable-next-line no-unused-expressions
     window.location.reload()
   }
 
-  render () {
-    return (
-      <div className='scan-book-wrapper'>
-        <PageTitler pagetitle='Scan Book' />
-        <Scanner updateBookId = {(bookID) => this.updateBookId(bookID)}/>
-        {this.state.book !== null ? 
-        <div className='scan-result-table'>
-          <button onClick={this.rescan} className='scan-result__restart'>Wrong title?</button>
-          <label className='scan-result__upc-label'>UPC</label>
-          <div className='scan-result__upc'>{this.state.upc}</div>
-          <label className='scan-result__title-label'>Title</label>
-          <div className='scan-result__title'>{this.state.title}</div>
-          <label className='scan-result__publisher-label'>Publisher</label>
-          <div className='scan-result__publisher'>{this.state.publisher}</div>
-          <label className='scan-result__author-label'>Author</label>
-          <div className='scan-result__author'>{this.state.author}</div>
-          <label className='scan-result__word-count-label'>Word Count</label>
-          <div className='scan-result__word-count'>{this.state.wordCount}</div>
-          <label className='scan-result__status-label'>Status</label>
-          <div className='scan-result__status'>{this.state.status}</div>
-          <label className='scan-result__current-holder-label'>Held by</label>
-          <div className='scan-result__current-holder'>{this.state.currentHolder}</div>
-            {this.state.currentHolder === "Onomichi Gakuen English Library" ? <SmallerGreenButton text='Check this book out' onClick={this.checkBookOut} />
-            :
-            <SmallerGreenButton text='Check this book back in' onClick={this.checkBookIn} />}
-        </div>
-          
-          : 
-          
-          null }
+  return (
+    <div className='scan-book-wrapper'>
+      <PageTitler pagetitle='Scan Book' />
+      <Scanner updateBookId = {(bookID) => updateBookId(bookID)}/>
+      {book.upc !== null ? 
+      <div className='scan-result-table'>
+        <button onClick={rescan} className='scan-result__restart'>Wrong title?</button>
+        <label className='scan-result__upc-label'>UPC</label>
+        <div className='scan-result__upc'>{book.upc}</div>
+        <label className='scan-result__title-label'>Title</label>
+        <div className='scan-result__title'>{book.title}</div>
+        <label className='scan-result__publisher-label'>Publisher</label>
+        <div className='scan-result__publisher'>{book.publisher}</div>
+        <label className='scan-result__author-label'>Author</label>
+        <div className='scan-result__author'>{book.author}</div>
+        <label className='scan-result__word-count-label'>Word Count</label>
+        <div className='scan-result__word-count'>{book.wordCount}</div>
+        <label className='scan-result__status-label'>Status</label>
+        <div className='scan-result__status'>{book.status}</div>
+        <label className='scan-result__current-holder-label'>Held by</label>
+        <div className='scan-result__current-holder'>{book.currentHolder}</div>
+          {book.currentHolder === "Onomichi Gakuen English Library" ? <SmallerGreenButton text='Check this book out' clickHandler={checkBookOut} />
+          :
+          <SmallerGreenButton text='Check this book back in' clickHandler={checkBookIn} />}
       </div>
-    );
-  }
+        
+        : 
+        
+        null }
+    </div>
+  );
 }
+export default ScanBookId;
