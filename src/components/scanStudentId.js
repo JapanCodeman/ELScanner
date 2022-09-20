ac787a2fa6e9a621625ff32250b7e0a32f46e43f
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2022-09-20 17:20:51 +0900

    added student display info to scanStudentId.js and started styling page. Also incorporated axios call for student ID QR code scan and adjusted state management to be more intuitive by separating book and student info into two separate state variables

--- src/components/scanStudentId.js
+++ src/components/scanStudentId.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { useLocation } from 'react-router';
 
@@ -7,39 +8,47 @@ import Scanner from './scanner';
 function ScanStudentId () {
   const location = useLocation()
 
-  const [student, updateInfo] = useState({
-    book: {
-      ...location.state
-    },
-    student: {
-      _id: "",
-      first: "",
-      last: "",
-      role: "Student",
-      class: "",
-      public_id: "",
-      email: "",
-      password: "",
-      loggedStatus: "",
-      isAdmin: "NOT_ADMIN"
-    }
+  const [book] = useState({
+    ...location.state
   })
+
+  const [student, updateInfo] = useState({
+    _id: "",
+    first: "",
+    last: "",
+    role: "Student",
+    class: "",
+    public_id: "",
+    email: "",
+    password: "",
+    loggedStatus: "",
+    isAdmin: "NOT_ADMIN"
+  })
+
+  const lookupUser = (public_id) => {
+    axios
+    .get(`http://127.0.0.1:5000/lookup-user/${public_id}`)
+    .then(response => {
+      updateInfo({...response.data}) 
+    })
+    .catch(error => {
+      console.log("There was an error in lookupUser function", error)
+    })
+  }
 
   return (
     <div>
       <PageTitler pagetitle="Student Id" />
-      <Scanner returnedInfo = {(studentInfo) => updateInfo(studentInfo)} />
-      <div className='student-info'>
-        {student.student._id}
-        {student.student.first}
-        {student.student.last}
-        {student.student.role}
-        {student.student.class}
-        {student.student.public_id}
-        {student.student.email}
-        {student.student.password}
-        {student.student.loggedStatus}
-        {student.student.isAdmin}
+      <Scanner returnedInfo = {(public_id) => lookupUser(public_id)} />
+      <div className='checkout-confirmation'>
+        <div className='checkout-confirmation__student-info'>
+          Student Name: {student.first} {student.last}
+          Class: {student.class}
+          Currently Checked Out Books: #Fill in later
+        </div>
+        <div className='checkout-confirmation__message'>
+          Check out title: {book.title} to {student.first} {student.last}?
+        </div>
       </div>
     </div>
   );
