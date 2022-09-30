dbcee5315b4935f63a8f9c30325d90a64ec089dc
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2022-09-30 16:59:02 +0900

    styled registerNewBooks.js; handleSubmit working

--- src/components/registerNewBook.js
+++ src/components/registerNewBook.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { useLocation } from 'react-router';
 import PageTitler from './helpers/pageTitler';
+import SmallerGreenButton from './helpers/smallerGreenButton';
 
   function RegisterNewBook() {
     const location = useLocation()
     
     const [bookInfo, setBookInfo] = useState({
-      ...location.state
+      ...location.state,
+      status: 'Checked in',
+      currentHolder: 'Onomichi Gakuen English Library'
     });
 
     const handleChange = (event) => {
@@ -17,23 +20,30 @@ import PageTitler from './helpers/pageTitler';
     const handleSubmit = (event) => {
       event.preventDefault();
       axios
-      .post(`http://127.0.0.1:5000/register-new-book/${bookInfo.upc}`)
+      .post(`http://127.0.0.1:5000/register-new-book/${bookInfo.upc}`, {...bookInfo})
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log('There was an error in the handleSubmit of registerNewBook.js', error)
+      })
     }
 
     return (
       <div className='register-book'>
         <PageTitler pagetitle='Register New Book' />
-        <form onSubmit={handleSubmit}>
-          <label className='register-book__upc-label'>UPC</label>
-          <input className='register-book__upc-input' type='text' name='upc' onChange={handleChange} defaultValue={bookInfo.upc} />
-          <label className='register-book__book-title-label'>Book Title</label>
-          <input className='register-book__book-title' type='text' name='title' onChange={handleChange} />
-          <label className='register-book__book-pub-label'>Publisher</label>
-          <input className='register-book__book-pub' type='text' name='publisher' onChange={handleChange} />
-          <label className='register-book__book-auth-label'>Author</label>
-          <input className='register-book__book-auth' type='text' name='author' onChange={handleChange} />
-          <label className='register-book__book-wordcount-label'>Word count</label>
-          <input className='register-book__book-wordcount' type='number' name='wordCount' onChange={handleChange} />
+        <form className='register-book-form' onSubmit={handleSubmit}>
+          <label className='register-book-form__upc-label'>UPC</label>
+          <input className='register-book-form__upc-input' type='text' name='upc' onChange={handleChange} defaultValue={bookInfo.upc} />
+          <label className='register-book-form__book-title-label'>Book Title</label>
+          <input className='register-book-form__book-title' type='text' name='title' onChange={handleChange} />
+          <label className='register-book-form__book-pub-label'>Publisher</label>
+          <input className='register-book-form__book-pub' type='text' name='publisher' onChange={handleChange} />
+          <label className='register-book-form__book-auth-label'>Author</label>
+          <input className='register-book-form__book-auth' type='text' name='author' onChange={handleChange} />
+          <label className='register-book-form__book-wordcount-label'>Word count</label>
+          <input className='register-book-form__book-wordcount' type='number' name='wordCount' onChange={handleChange} />
+          <SmallerGreenButton className='register-book-form__submit-button' text='Register New Book' typeSet='submit' clickHandler={handleSubmit} />
         </form>
       </div>
     );
