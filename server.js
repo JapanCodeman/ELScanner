c1937610472a933b023d1c9fe1b885ea1de34f21
JapanCodeman <ryansurdick@MacBook-Pro.local>
2022-10-31 14:52:54 +0900

    server.js restored again

--- server.js
+++ server.js
@@ -12,22 +12,22 @@
 
 
 // .get from old; rest from new
-// const express = require('express');
-// const favicon = require('express-favicon');
-// const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//   return res.send('pong');
-// });
-// app.get(/.*/, function (req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// })
-// app.listen(port);
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+app.listen(port);
 
 // original below
 // const express = require('express');
