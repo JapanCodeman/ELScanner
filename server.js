fcbdea491747efc94b9c18a39be828db0da63ec9
JapanCodeman <ryansurdick@MacBook-Pro.local>
2022-10-31 16:37:17 +0900

    changed app.use(express.static())

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'public')))
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
