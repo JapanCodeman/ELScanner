d28c5578e8011dbd8109ad5b1e373c1ebd862864
JapanCodeman <ryansurdick@MacBook-Pro.local>
2022-10-31 16:17:43 +0900

    changed res.sendFile args to use path.resolve

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 app.get('*', function (req, res) {
-  res.sendFile(__dirname + './build/index.html');
+  // res.sendFile(__dirname + './public/index.html');
+  res.sendFile(path.resolve("./public", "index.html"));
 })
 app.listen(port);
