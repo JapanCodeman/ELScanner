5db242f51c01ffc6e2dd39db0409d53bcf35d505
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2022-11-11 16:33:50 +0900

    trying redirect in server.js

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-import sslRedirect from 'heroku-ssl-redirect';
+// import sslRedirect from 'heroku-ssl-redirect';
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
 app.use(favicon(__dirname + '/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(sslRedirect());
+// app.use(sslRedirect());
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
