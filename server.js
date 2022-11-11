35abdc58cae8e16fd75e10b12b6c3d2ab2dc18a4
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2022-11-11 15:50:59 +0900

    reinstalling express sslify

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
+const enforce = require('express-sslify');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -8,6 +9,7 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
